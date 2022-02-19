ec52b766f7e87c91e31e8ff58cbadb9af7edce70
Lee <93831345+Hyfreadom@users.noreply.github.com>
2022-02-19 18:29:30 +0800

    Update convert.py

--- convert.py
+++ convert.py
@@ -2,32 +2,25 @@ import os
 import cv2
 import numpy as np
 import struct
-def save_mnist_to_jpg(dataset_path, output_path,label_path): 
-    files = os.listdir(dataset_path)
-    print(files)
-    #make image_file path
-    mnist_image_file = os.path.join(dataset_path, [f for f in files if "image" in f][0])
-    #make label_file path 
-    mnist_label_file = os.path.join(dataset_path, [f for f in files if "label" in f][0])
-    save_dir = output_path
-    prefix = 'test'
-    #read in binary
-    with open(mnist_image_file, 'rb') as f1: 
-        #head_file=f1.read(16)# head_inf, read by the following function
+def save_mnist_to_jpg(image_data,label_data,output_path,label_path,kind): 
+
+    with open(image_data, 'rb') as f1: 
         magic_number, num_file, height, width = struct.unpack('>IIII',f1.read(16))
         size = height*width
         image_file = f1.read()
-    with open(mnist_label_file, 'rb') as f2:
+
+    with open(label_data, 'rb') as f2:
         magic_number,num_file = struct.unpack('>II',f2.read(8))       
-        print(num_file)
         label_file = f2.read()
-    img_label=open(label_path+'-label','w+')
+
+    img_label=open(label_path,'w+')
     for i in range(num_file):
         label = label_file[i]
         image_list = [item for item in image_file[i * size:i * size + size]]
         image_np = np.array(image_list, dtype=np.uint8).reshape(height, width)
-        save_name = os.path.join(save_dir, '{}_{}_{}.jpg'.format(prefix, i, label))
+        save_name = os.path.join(output_path, '{}_{}_{}.jpg'.format(kind, i, label))
         cv2.imwrite(save_name, image_np)
+
 
         label_name=save_name+"   "+str(label)+'\n'
         img_label.write(label_name)
@@ -35,5 +28,15 @@ def save_mnist_to_jpg(dataset_path, output_path,label_path):
     print("=" * 20, "preprocess data finished", "=" * 20)
  
 if __name__ == '__main__':
-    save_mnist_to_jpg('./data/MNIST/test/','./data/MNIST/test_picture/','./test')
-    save_mnist_to_jpg('./data/MNIST/train/','./data/MNIST/train_picture/','./train')
+    #压缩的图片文件所在位置
+    image_data = "D:\\OneDrive\\python_work\\data\\MNIST\\raw\\train-images-idx3-ubyte"
+    #压缩的标签文件所在位置
+    label_data = "D:\\OneDrive\\python_work\\data\\MNIST\\raw\\train-labels-idx1-ubyte" 
+    #转换的图片所在位置,注意,'/'一定要加在这里! '/'一定要加在这里! '/'一定要加在这里! 否则可能存到根目录
+    output_path='./train_data/picture/'       
+    #标签文件所在位置
+    label_path="./train_data/label.txt"  
+    #数据集类型用于给图片起名
+    kind='train'                                             
+    save_mnist_to_jpg(image_data,label_data,output_path,label_path,kind)
+
